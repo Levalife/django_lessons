74a4bbf03bab96604914b172f8a1a897a618905a
levushka14@gmail.com <levushka14@gmail.com>
2013-06-27 12:00:24 +0300

    dynamic urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,12 +1,18 @@
 from django.conf.urls import patterns, include, url
-from mysite.views import hello
+from mysite.views import hello, current_datetime, hours_ahead
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^hello/$', hello),
+	url(r'^hello/$', hello), 
+	url(r'^time/$', current_datetime),
+	#url(r'^time/plus/1/$', one_hour_ahead),
+	#url(r'^time/plus/2/$', two_hours_ahead),
+	#url(r'^time/plus/3/$', three_hours_ahead),
+	#url(r'^time/plus/4/$', four_hours_ahead),
+	url(r'^time/plus/(\d{1,2})/$', hours_ahead),
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^mysite/', include('mysite.foo.urls')),
